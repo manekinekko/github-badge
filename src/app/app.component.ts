0dba8ce0ac6aeb10a2219bb9016b8c17dc183df5
Wassim Chegham <github@wassim.dev>
2020-06-09 18:48:53 +0200

    chore: update app header title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { AuthService } from "./auth.service";
+import { AuthService } from "./auth0.service";
 
 @Component({
   selector: "app-root",
@@ -7,9 +7,9 @@ import { AuthService } from "./auth.service";
     <header>
       <a routerLink="/">
         <img src="assets/angular-logo.svg" width="28px" />
+        <span>GITHU</span><span>BADGE</span>
       </a>
       <span></span>
-      <button (click)="auth.login('/profile')" *ngIf="!auth.loggedIn">Log In</button>
       <button (click)="auth.logout()" *ngIf="auth.loggedIn">Log Out</button>
     </header>
     <router-outlet></router-outlet>
@@ -20,17 +20,34 @@ import { AuthService } from "./auth.service";
         height: 100%;
         width: 100%;
       }
+      
       header {
         background-color: black;
         color: white;
         display: flex;
         padding: 10px;
       }
+      
       header span {
         flex: 1;
       }
+
       header a {
         color: white;
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+      }
+
+      header span:last-child {
+        color: lightcoral;
+      }
+
+      button {
+        border: 1px solid white;
+        background: transparent;
+        color: white;
+        padding: 0 15px;
       }
     `,
   ],
